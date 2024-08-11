f7066088955b2a4d701834b36da8669e4acfcb72
Nate Macarelli <33464190+natemac47@users.noreply.github.com>
2024-08-11 19:52:24 -0400

    Update brCondTCH.js

--- brCondTCH.js
+++ brCondTCH.js
@@ -1,1 +1,1 @@
-current.state.changesTo(3) && !gs.getUser().isMemberOf("IS Inventory") || current.state.changesTo(3) && current.assignment_group !== "IS Inventory" || current.state.changesTo(3) && u_confirmation_that_i_have_updated_the_assignment_group_of_this_task_to_is_inven == false
+current.state.changesTo(3) && !gs.getUser().isMemberOf("IS Inventory") || current.state.changesTo(3) && current.assignment_group !== "IS Inventory" || current.state.changesTo(3) && u_conf == false
